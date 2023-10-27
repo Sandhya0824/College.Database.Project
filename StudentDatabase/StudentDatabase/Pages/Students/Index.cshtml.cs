54cf712305c2ee6863d5973e8d8d2f4de944692d
manaswiniray <manaswiniray2002@gmail.com>
2023-10-28 01:08:42 +0530

    Updated

--- StudentDatabase/StudentDatabase/Pages/Students/Index.cshtml.cs
+++ StudentDatabase/StudentDatabase/Pages/Students/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Net;
 using System.Reflection;
 using System.Xml.Linq;
+using System.Collections.Generic;
 
 namespace StudentDatabase.Pages.Students
 {
@@ -12,43 +13,44 @@ namespace StudentDatabase.Pages.Students
     {
         public StudentInfo studentInfo1=new StudentInfo();
         public List<StudentInfo> studentList=new List<StudentInfo>();
-        public IActionResult OnGet()
+        public void OnGet()
         {
             try
             {
-                String connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CollegeLoginPortal;Integrated Security=True;Connect Timeout=30;Encrypt=False";
+                String connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CollegeLoginPortal;Integrated Security=True";
 
 				using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    String sql = "select * from student";
+                    String sql = "SELECT * FROM Student";
                     using(SqlCommand command= new SqlCommand(sql, connection))
                     {
                         using(SqlDataReader reader = command.ExecuteReader())
                         {
-                            while(reader.Read())
-                            {
-                                StudentInfo studentInfo = new StudentInfo
-                                {
-                                    RegdNo = "" + reader.GetInt32(0),
-                                    Name = reader.GetString(1),
-                                    DOB = reader.GetString(2),
-                                    Gender = reader.GetString(3),
-                                    Address = reader.GetString(4),
-                                    CourseId = "" + reader.GetInt32(5)
-                                };
+							while (reader.Read())
+							{
+								StudentInfo studentInfo = new StudentInfo
+								{
+									RegdNo = "" + reader.GetInt32(0),
+									Name = reader.GetString(1),
+									DOB = reader.GetString(2),
+									Gender = reader.GetString(3),
+									Address = reader.GetString(4),
+									CourseId = "" + reader.GetInt32(5)
+								};
 
 								studentList.Add(studentInfo);
-                            }
-                        }
-                    }
+							}
+
+						}
+					}
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Error Occurred: " + ex.Message);
             }
-            return Page();
+            
         }
     }
 
