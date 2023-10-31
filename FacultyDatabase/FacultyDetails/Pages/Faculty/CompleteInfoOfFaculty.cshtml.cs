cc2db0b9ca82b6ca1aff59c32d19acb0a3870f3a
Aakriti Kumari <kumariaakriti1404@gmail.com>
2023-10-31 10:18:33 +0530

    Committed for FacultyDatabase for CRUD

--- FacultyDatabase/FacultyDetails/Pages/Faculty/CompleteInfoOfFaculty.cshtml.cs
+++ FacultyDatabase/FacultyDetails/Pages/Faculty/CompleteInfoOfFaculty.cshtml.cs
@@ -6,8 +6,7 @@ namespace FacultyDatabase.Pages.Faculty
 {
     public class CompleteInfoOfFacultyModel : PageModel
     {
-        public FacultyInfo FacultyInfo { get; set; }
-        public string ErrorMessage { get; set; }
+        public FacultyInfo FacultyInfo = new FacultyInfo(); public string ErrorMessage { get; set; }
         public void OnGet(string facultyId)
         {
             if (string.IsNullOrEmpty(facultyId))
@@ -32,17 +31,22 @@ namespace FacultyDatabase.Pages.Faculty
                     {
                         if (reader.Read())
                         {
-                            FacultyInfo = new FacultyInfo
+                            FacultyInfo.FacultyId = reader.GetInt32(0).ToString();
+                            FacultyInfo.Name = reader.GetString(1);
+                            FacultyInfo.DOB = reader.GetString(2);
+                            FacultyInfo.Gender = reader.GetString(3);
+                            FacultyInfo.Address = reader.GetString(4);
+                            FacultyInfo.CourseId = reader.GetInt32(5).ToString();
+                            // Check if the ProfilePhoto field is null
+                            if (!reader.IsDBNull(6))
                             {
-                                FacultyId = reader.GetInt32(0).ToString(),
-                                Name = reader.GetString(1),
-                                DOB = reader.GetString(2),
-                                Gender = reader.GetString(3),
-                                Address = reader.GetString(4),
-                                CourseId = reader.GetInt32(5).ToString(),
-                                
-                                
-                            };
+                                FacultyInfo.ProfilePhoto = (byte[])reader.GetValue(6);
+                            }
+                            else
+                            {
+                                // Handle the case where ProfilePhoto is null (e.g., assign a default photo or set it to an empty byte[])
+                                FacultyInfo.ProfilePhoto = new byte[0]; // You can choose how to handle this case.
+                            }
                         }
                         else
                         {
